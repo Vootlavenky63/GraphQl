475945d984a2b0591e24a05580c37792ddd99b77
vootlavenky63 <vootla.venky63@gmail.com>
2019-09-17 17:39:44 +0530

    GraphQl fullStack BAckEnd

--- index.js
+++ index.js
@@ -4,21 +4,74 @@ const { GraphQLServer } = require('graphql-yoga')
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/TodoGraphQL', {useNewUrlParser: true,useUnifiedTopology: true},() => {
     console.log("mongodb running");
+
     
 }
 );
+var Todo = mongoose.model('Todo', {
+    text:String,
+    completed:Boolean
+
+});
+
 
 
 const typeDefs = `
+type Todo {
+    id:ID!
+    text: String!
+    completed:Boolean!           
+  }
   type Query {
     hello(name: String): String!
+    todos: [Todo]
+   findTodo(id:ID):Todo
+
+    
+
+  }
+
+  type Mutation {
+    createTodo(text:String): Todo
+    updateTodo(text:String,completed:Boolean,id:ID): String
+    deleteTodo(id:ID): String
+
+
   }
 `
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`, //string litteral
-    hello: (name) => "hello world", //string litteral
+    hello: (_, { name }) => `Hello ${name || 'World'}`,          
+    hello: (name) => "hello world",             //string litteral
+    todos:() => Todo.find(),
+    findTodo: async (_, {id}) => {
+
+       return await Todo.findById(id)
+        // return true
+    },   
+
+        
+    
+
+  },
+  Mutation: {
+    createTodo: async (_, { text }) => {
+        const todo=new Todo({text,completed:false}) 
+        await todo.save()
+        return todo
+    }, 
+    updateTodo: async (_, {id,text,completed }) => {
+
+        await Todo.findByIdAndUpdate(id,{text,completed})
+        return true
+    },   
+    deleteTodo: async (_,{id}) => {
+
+        await Todo.findByIdAndDelete(id)
+        return true
+    },               
+        
 
   },
 }
